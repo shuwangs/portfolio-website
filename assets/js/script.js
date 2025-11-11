dd045236d777d38ab67049c43f652c53c710f087
Shu <shuw425@gmail.com>
2025-11-11 00:10:59 -0500

    Modify header style

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+/*
+  TODOS:
+  1. search the string in the webpage that contains the input string.- highligh it
+*/
+// document.querySelector(".search_form").addEventListener('submit', (event) =>{
+//   let query = document.getElementById('search_input').value.trim();
+//   if (query !== "") {
+//     let content = document.querySelectorAll(body).innerHTML;
+
+//   }
+
+// })
