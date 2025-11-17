dd90bf1d1dae89c5a1184952cf2f2d31c77606f1
Shu <shuw425@gmail.com>
2025-11-16 22:09:47 -0500

    Style the responsive by media - add education information, - add work experience information - change the style of about me and projects section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 /*
   TODOS:
   1. search the string in the webpage that contains the input string.- highligh it
+  2. Form Validation for contact me form
 */
 // document.querySelector(".search_form").addEventListener('submit', (event) =>{
 //   let query = document.getElementById('search_input').value.trim();
@@ -10,3 +11,6 @@
 //   }
 
 // })
+
+
+//  2. Form Validation for contact me form
