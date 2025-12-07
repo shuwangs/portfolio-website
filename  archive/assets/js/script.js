756265566752ce36e89b6325c94e05ef8a258581
Shu <shuw425@gmail.com>
2025-12-06 21:28:32 -0500

    put original folder into archive

---  archive/assets/js/script.js
+++  archive/assets/js/script.js
@@ -3,20 +3,20 @@
   1. search the string in the webpage that contains the input string.- highligh it
   2. Form Validation for contact me form
 */
-// document.querySelector(".search_form").addEventListener('submit', (event) =>{
-//   let query = document.getElementById('search_input').value.trim();
-//   if (query !== "") {
-//     let content = document.querySelectorAll(body).innerHTML;
 
-//   }
 
-// })
+
+document.addEventListener("DOMContentLoaded", () => {
+
+//  1. search the string in the webpage that contains the input string.- highligh it
+
+
 
 
 // 3. toggle light button
-document.addEventListener("DOMContentLoaded", () => {
   const themeBtn = document.getElementById("theme-toggle");
   const body = document.body;
+
 
   if (!themeBtn) {
     return;
