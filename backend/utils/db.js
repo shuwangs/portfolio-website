e30f6766bf795ac89f4f24a89d318c79cd69a2bd
Shu <shuw425@gmail.com>
2026-01-13 23:53:29 -0500

    changed dotconfig path to  ../.env

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,7 +1,8 @@
 import {Pool} from 'pg';
 import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config("../.env");
+console.log(process.env.DATABASE_URL);
 
 if(!process.env.DATABASE_URL) {
     throw new Error("Missing DATABASE_URL in .env");
